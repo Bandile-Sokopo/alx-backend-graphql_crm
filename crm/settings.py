d90b818c5b28780e26023d08af92c778e4151770
Bandile <133995097+Bandile-Sokopo@users.noreply.github.com>
2025-11-02 20:40:24 +0200

    - Directory Update

--- crm/settings.py
+++ crm/settings.py
@@ -2,6 +2,22 @@ from celery.schedules import crontab
 
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+    'django_filters',
+    'crm',
+    'graphene_django',
+    'django_filters',
+    'django_crontab',
+    'django_celery_beat',
+]
+
 CELERY_BEAT_SCHEDULE = {
     'generate-crm-report': {
         'task': 'crm.tasks.generate_crm_report',
